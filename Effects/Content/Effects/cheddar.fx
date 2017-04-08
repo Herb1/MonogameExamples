0f9213c17585ed1e789e0adbff46e532aeab5dc6
Gerd Schmidt <gerd@acagamics.de>
2017-04-08 15:27:36 +0200

    Added example effect project.

---
+++ Effects/Content/Effects/cheddar.fx
@@ -0,0 +1,118 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+
+float scanHeight;
+
+float3 LightDir = float3(0, 1, -1);
+
+float4 LightColor = float4(1, 1, 1, 1);
+float intensity = 0.6;
+
+texture tex;
+texture tex2;
+
+float4 AmbientColor;
+
+texture ModelTextureA;
+sampler2D textureSamplerA = sampler_state {
+	Texture = (ModelTextureA);
+	MinFilter = Linear;
+	MagFilter = Linear;
+	AddressU = Clamp;
+	AddressV = Clamp;
+};
+
+texture ModelTextureB;
+sampler2D textureSamplerB = sampler_state {
+	Texture = (ModelTextureB);
+	MinFilter = Linear;
+	MagFilter = Linear;
+	AddressU = Clamp;
+	AddressV = Clamp;
+};
+
+
+struct VertexShaderInput
+{
+	float4 Position : POSITION0;
+	float2 TexCoords : TEXCOORD0;
+	float4 Normal : NORMAL0;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : POSITION0;
+	float2 TexCoords : TEXCOORD0;
+	float4 Normal : TEXCOORD1;
+	float3 PiPos: TEXCOORD2;
+};
+
+VertexShaderOutput VertexShaderFunction(VertexShaderInput input)
+{
+	VertexShaderOutput output;
+
+	float4 worldPosition = mul(input.Position - input.Normal * 0.1 * scanHeight * 2, World);
+	float4 viewPosition = mul(worldPosition, View);
+	output.Position = mul(viewPosition, Projection);
+
+
+	output.PiPos = mul(viewPosition, Projection);
+
+	output.TexCoords = input.TexCoords;
+	output.Normal = input.Normal;
+
+
+
+	return output;
+}
+
+float4 PixelShaderFunction(VertexShaderOutput input) : COLOR
+{
+	//Read from texture
+	float4 colorInTexture = tex2D(textureSamplerA, input.TexCoords);
+	float4 colorInTexture2 = tex2D(textureSamplerB, input.TexCoords);
+
+	//merged textures
+	//float4 newTexColor = (1 - scanHeight)*colorInTexture + scanHeight * colorInTexture2;
+	//return newTexColor;
+
+	//Light
+	float4 normal = input.Normal;
+	float4 diffuse = saturate(dot(-LightDir, normal));
+
+	float4 texColorWithLightAbove = colorInTexture * 0.1 + 0.9 * (colorInTexture * LightColor * intensity * diffuse);
+	// lerp(x,y,factor)
+
+	float4 texColorWithLightUnder = colorInTexture2*0.1 + 0.9*(colorInTexture2 * LightColor * intensity * diffuse);
+
+	//Scannline part
+	float pos = (float)input.PiPos.y;
+
+	if (pos < -4.5 + (scanHeight * 10))
+		return texColorWithLightAbove;
+	else if (pos > -4.5 + scanHeight * 10 && pos < -4.35 + scanHeight * 10)
+		return lerp(float4(1, 0, 0, 1), texColorWithLightAbove, 0.4);
+	else
+		return texColorWithLightUnder;
+
+}
+
+technique BasicColorDrawing
+{
+	pass P0
+	{
+		VertexShader = compile VS_SHADERMODEL VertexShaderFunction();
+		PixelShader = compile PS_SHADERMODEL PixelShaderFunction();
+	}
+};
---
+++ Effects/Game1.cs
@@ -0,0 +1,139 @@
+﻿namespace Effects
+{
+    using System;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        #region member vars
+
+        private SpriteBatch _spriteBatch;
+
+        private Vector3 _lookAt;
+
+        private Model _model;
+        private Matrix _view;
+        private Matrix _projection;
+
+        private Texture2D _texturePandaOriginal;
+        private Texture2D _texturePandaZombie;
+
+        private float _angle;
+        private float _scanHeight;
+
+        private Matrix _world;
+
+        private Effect _effect;
+        private GraphicsDeviceManager _graphics;
+
+        #endregion
+
+        #region constructors and destructors
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            Window.Title = "Effects";
+        }
+
+        #endregion
+
+        #region methods
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            /*foreach (ModelMesh mesh in _model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                    effect.World = _world * mesh.ParentBone.Transform * Matrix.CreateTranslation(new Vector3(0, -1.5f, 0));
+                    effect.View = _view;
+                    effect.Projection = _projection;
+                }
+                mesh.Draw();
+            }*/
+
+            DrawModelWithEffect(_model, _world, _view, _projection, _effect);
+            base.Draw(gameTime);
+        }
+
+        protected override void Initialize()
+        {
+            _projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 600f, 0.1f, 100f);
+
+            _view = Matrix.CreateLookAt(new Vector3(0, 1, 10), new Vector3(0, -100, 0), new Vector3(0, 1, 0));
+
+            _world = Matrix.CreateScale(0.02f);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _model = Content.Load<Model>("panda");
+            _effect = Content.Load<Effect>("Effects/cheddar");
+
+            _texturePandaOriginal = Content.Load<Texture2D>("Textures/panda_original");
+            _texturePandaZombie = Content.Load<Texture2D>("Textures/panda_zombie");
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _scanHeight = ((float)Math.Sin(gameTime.TotalGameTime.TotalMilliseconds / 1000) + 1) / 2f;
+
+            _angle += 0.001f * gameTime.ElapsedGameTime.Milliseconds;
+            _lookAt = new Vector3((float)Math.Sin(_angle), 0, (float)Math.Cos(_angle));
+
+            _view = Matrix.CreateLookAt(_lookAt * 10, new Vector3(0, 0, 0), new Vector3(0, 1, 0));
+
+            base.Update(gameTime);
+        }
+
+        private void DrawModelWithEffect(Model model, Matrix world, Matrix view, Matrix projection, Effect myEffect)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    part.Effect = myEffect;
+                    myEffect.Parameters["World"].SetValue(world * mesh.ParentBone.Transform * Matrix.CreateTranslation(new Vector3(0, -1.5f, 0)));
+                    myEffect.Parameters["View"].SetValue(view);
+                    myEffect.Parameters["Projection"].SetValue(projection);
+
+                    //myEffect.Parameters["AmbientColor"].SetValue(new Vector4(1, 1, 0, 1));
+                    myEffect.Parameters["ModelTextureA"].SetValue(_texturePandaOriginal);
+                    myEffect.Parameters["ModelTextureB"].SetValue(_texturePandaZombie);
+
+                    myEffect.Parameters["scanHeight"].SetValue(_scanHeight);
+                }
+                mesh.Draw();
+            }
+        }
+
+        #endregion
+    }
+}
